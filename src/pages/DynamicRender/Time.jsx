44db0d0c5e4cf1ab4c290315fee14aff2b4a286c
Riyan Irfan <rirfan@axisdatalytics.com>
2025-03-27 03:12:13 +0500

    Added Timer into Time.jsx

--- src/pages/DynamicRender/Time.jsx
+++ src/pages/DynamicRender/Time.jsx
@@ -1,34 +1,47 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 const Time = () => {
-    const mytime = setInterval(()=> { (new Date().toISOString());}, 1000); 
-    console.log(mytime)
-    const handleTime = () => {
+  // const mytime = setInterval(()=> { (new Date().toISOString());}, 1000);
+  // console.log(mytime)
+  // const handleTime = () => {
 
-    }
+  // }
+
+  const [myTime, setMyTime] = useState(new Date());
+
+  setInterval(function () {
+    setMyTime(new Date());
+  }, 1000); // 1000 ms = 1 second
+
+  console.log(myTime.getHours(), myTime.getMinutes(), myTime.getSeconds());
 
   return (
     <>
-
-<div style={{ backgroundColor:'#363636', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent:'center', height: '100vh' }}>
-
-    <div style={{display:'flex', gap: '185px', fontSize: '20px', color: '#FCE014'}}>
-    <h3>HOURS</h3>
-    <h3>MINUTES</h3> 
-    <h3>SECONDS</h3>
-    </div>
-    <div style={{display:'flex', gap: '100px', fontSize: '35px', color: '#FCE014', fontFamily: 'arial'}}>
-        <h1>00</h1>
-        <h1>:</h1>
-        <h1>00</h1>
-        <h1>:</h1>
-        <h1>00</h1>
-    </div>
-</div>
-
-
+      <div
+        style={{
+          backgroundColor: "#363636",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          height: "100vh",
+        }}
+      >
+        <div style={{ display: "flex", gap: "185px", fontSize: "20px", color: "#FCE014" }}>
+          <h3>HOURS</h3>
+          <h3>MINUTES</h3>
+          <h3>SECONDS</h3>
+        </div>
+        <div style={{ display: "flex", gap: "100px", fontSize: "35px", color: "#FCE014", fontFamily: "arial" }}>
+          <h1>{myTime.getHours()}</h1>
+          <h1>:</h1>
+          <h1>{myTime.getMinutes()}</h1>
+          <h1>:</h1>
+          <h1>{myTime.getSeconds()}</h1>
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Time
+export default Time;
