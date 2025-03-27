0a52e1eff8cecece778f5eefc1811d899b249b09
Riyan Irfan <rirfan@axisdatalytics.com>
2025-03-28 02:51:32 +0500

    Added Error Message and Filter function for Delete Todo

--- src/pages/Todo/Todo.jsx
+++ src/pages/Todo/Todo.jsx
@@ -1,10 +1,8 @@
-import React, { useState } from 'react'
-import TrashSvg from './TrashSvg'
-
-
+import React, { useState } from "react";
+import TrashSvg from "./TrashSvg";
 
 const Todo = () => {
-  const [text, setText] = useState('');
+  const [text, setText] = useState("");
   const [todos, setTodos] = useState([]);
 
   console.log(todos);
@@ -16,50 +14,69 @@ const Todo = () => {
     const newTodo = [...todos];
     newTodo.push(text);
     setTodos(newTodo);
-    setText('');
-  }
+    setText("");
+  };
 
   const deleteTodo = (index) => {
-    const newTodos = [];
-    for (let i = 0; i < todos.length; i++) {
-      if (i !== index) {
-        newTodos.push(todos[i]);
-      }
-    }
+    // const newTodos = [];
+    // for (let i = 0; i < todos.length; i++) {
+    //   if (i !== index) {
+    //     ['Hello', '2', '3']
+    //     newTodos.push(todos[i]);
+    //   }
+    // }
+    const newTodos = todos.filter((item, ind) => {
+      return index !== ind;
+    });
     setTodos(newTodos);
   };
-  
-  
 
   return (
-    <main className='bg-dark text-light d-flex justify-content-center align-items-center' style={{ height: '100vh' }}>
-        <style>{`
+    <main className="bg-dark text-light d-flex justify-content-center align-items-center" style={{ height: "100vh" }}>
+      <style>{`
             ::placeholder {
                 color: gray !important;
             }
         `}</style>
-       <div className='rounded-3 px-3 py-5' style={{ width: '40rem', backgroundColor: '#1E1E1E' }}>
-          <h2 className='text-center'>TODO</h2>
-          <div className='d-flex gap-3 mt-5'>
-            <input type="text" onChange={(e) => {
-                setText(e.target.value);
-            }} value={text} className='form-control text-light' placeholder='Enter a task' style={{ backgroundColor: '#2C2C2C' }}/>
-            <button className='btn btn-primary' onClick={addTodo}>Add</button>
-          </div>
+      <div className="rounded-3 px-3 py-5" style={{ width: "40rem", backgroundColor: "#1E1E1E" }}>
+        <h2 className="text-center">TODO</h2>
+        <div className="d-flex gap-3 mt-5">
+          <input
+            type="text"
+            onChange={(e) => {
+              setText(e.target.value);
+            }}
+            value={text}
+            className="form-control text-light"
+            placeholder="Enter a task"
+            style={{ backgroundColor: "#2C2C2C" }}
+          />
+          <button className="btn btn-primary" onClick={addTodo}>
+            Add
+          </button>
+        </div>
+        {todos.length === 0 && <p className="text-center text-danger mt-3">No Todos</p>}
         {todos.map((todo, index) => {
-          return <div key={index}>
-            <div className='d-flex justify-content-between align-items-center mt-4'>
+          return (
+            <div key={index}>
+              <div className="d-flex justify-content-between align-items-center mt-4">
                 <span>{todo}</span>
-                <button className='btn' onClick={()=>{deleteTodo(index)}}>
-                    <TrashSvg /> 
+                <button
+                  className="btn"
+                  onClick={() => {
+                    deleteTodo(index);
+                  }}
+                >
+                  <TrashSvg />
                 </button>
+              </div>
+              <hr />
             </div>
-            <hr />
-            </div>
+          );
         })}
-       </div>
+      </div>
     </main>
-  )
-}
+  );
+};
 
-export default Todo
+export default Todo;
